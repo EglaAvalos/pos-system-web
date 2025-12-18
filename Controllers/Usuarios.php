1423b6e85ecd0fcc28fde88a0eef2afe51fdfcf2
EglaAvalos <avalosegla1dm@gmail.com>
2025-12-18 09:36:47 -0700

    Modificaciones en el login, y validacion de datos

---
+++ Controllers/Usuarios.php
@@ -0,0 +1,12 @@
+<?php
+class Usuarios extends Controller{
+    public function index ()
+    {
+        print_r($this->model->getUsuario());
+    }
+    public function validar(){
+        print_r($_POST);
+        die();
+    }
+}
+?>
--- Views/index.php
+++ Views/index.php
@@ -20,17 +20,17 @@
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header"><h3 class="text-center font-weight-light my-4">Iniciar Sesión</h3></div>
                                     <div class="card-body">
-                                        <form>
+                                        <form id="frmLogin">
                                             <div class="form-group">
-                                                <label class="small mb-1" for="inputEmailAddress"><i class="fas fa-user"></i> Usuario</label>
-                                                <input class="form-control py-4" id="inputEmailAddress" type="text" placeholder="Ingrese Usuario" />
+                                                <label class="small mb-1" for="usuario"><i class="fas fa-user"></i> Usuario</label>
+                                                <input class="form-control py-4" id="usuario" name="usuario" type="text" placeholder="Ingrese Usuario" />
                                             </div>
                                             <div class="form-group">
-                                                <label class="small mb-1" for="inputPassword"><i class="fas fa-key"></i>Contraseña</label>
-                                                <input class="form-control py-4" id="inputPassword" type="password" placeholder="Ingrese Contraseña" />
+                                                <label class="small mb-1" for="clave"><i class="fas fa-key"></i>Contraseña</label>
+                                                <input class="form-control py-4" id="clave" name="clave" type="password" placeholder="Ingrese Contraseña" />
                                             </div>
                                             <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <a class="btn btn-primary" href="index.html">Login</a>
+                                                <button class="btn btn-primary" type="submit" onclick="frmLogin(event)">Login</button>
                                             </div>
                                         </form>
                                     </div>
@@ -44,7 +44,7 @@
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
+                            <div class="text-muted">Copyright &copy; Egla Avalos 2025</div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;
@@ -58,5 +58,9 @@
         <script src="<?php echo base_url; ?>Assets/js/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
         <script src="<?php echo base_url; ?>Assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="<?php echo base_url; ?>Assets/js/scripts.js"></script>
+        <script>
+            const base_url = "<?php echo base_url; ?>";
+        </script>
+        <script src="<?php echo base_url; ?>Assets/js/funciones.js"></script>
     </body>
 </html>
